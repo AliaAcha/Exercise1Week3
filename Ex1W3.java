34635983732f341f617fe24de49c89b3501199c7
AliaAcha <72845206+AliaAcha@users.noreply.github.com>
2020-10-28 10:38:33 +0800

    Add files via upload

---
+++ Ex1W3.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+public class Ex1W3
+{
+	public static void main (String [] nilaiTerima)
+	{
+		int noInt = 10, x ;
+
+		/*if(noInt == 10)
+			System.out.println("sama");
+		else 
+			System.out.println("tak sama");*/
+
+		System.out.println(noInt == 10 ? "sama" : "tak sama");
+
+		if(noInt == 10)
+			x = 3;
+		else 
+			x = 19;
+
+		//ternary operator
+		x = noInt == 10 ? 3 : 19 ;
+
+		System.out.println("x is :" + x);
+
+
+		/*String nama;
+		int no;
+		double noDou;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter a name: ");
+		nama = input.nextLine(); //next() akan keluar kan 1 je
+		System.out.println("Your name is :" + nama);
+
+		System.out.println("Enter int number: ");
+		no = input.nextInt();
+		System.out.println("Integer number is :" + no);*/
+
+		/*input.nextLine();/*
+
+		/*System.out.println("Enter a name: ");
+		nama = input.nextLine(); //nextLine akan terima semua 
+		System.out.println("Your name is :" + nama);*/
+
+		/*System.out.println("Enter double number: ");
+		noDou = input.nextDouble();
+		System.out.println("Double number is :" + noDou);
+
+		input.close();*/
+
+
+		/*int [] tataInt = {10, 20, 30, 40};
+
+		//ordinary for
+		for (int i = 0; i < tataInt.length; i++)
+		{
+			System.out.println(tataInt[i]);
+		}
+
+		//for each or advanced for
+		for ( int x : tataInt )
+		{
+			System.out.println("for each: " + x );
+		}*/
+
+
+		/*final static char CHARSAYA = 53 ; //asci code
+		System.out.println("Char " + CHARSAYA);
+		CHARSAYA = 100;
+		System.out.println("Char " + CHARSAYA);*/ //cari ni final static / static final
+
+
+		/*char charSaya = 53 ; //asci code
+		System.out.println("Char " + charSaya);*/
+
+
+		/*int noInt = 0; //declare and initialize 
+		System.out.println("Nombor int " + noInt);
+		int [] tataInt = new int [4]; //declare
+		for (int i = 0; i < tataInt.length; i++)
+		{
+			System.out.println(tataInt[i]);
+		}*/
+
+
+		/*int noInt = 123;
+		int noInt2 = 343;
+		double noDouble = 123.456;
+
+		System.out.format("%6d dan %4d", noInt , noInt2);
+		System.out.format("%4d", noInt2);
+		System.out.format("RM%7.2f", noDouble);
+		System.out.printf("\nRM%7.3f dan %6d", noDouble, noInt);*/
+
+
+		/*System.out.println("Nilai 1: " + nilaiTerima[0]);
+		System.out.println("Nilai 1: " + nilaiTerima[1]);
+		System.out.println("Nilai 1: " + nilaiTerima[2]);*/
+
+
+		/*int sum = 0;
+
+		for(int i = 0; i < nilaiTerima.length; i++)
+		{
+			System.out.println("Nilai 1: " + nilaiTerima[i]);
+			sum += Integer.parseInt(nilaiTerima[i]);
+			System.out.println("Nilai sum: " + sum);
+		}*/
+	}
+}
